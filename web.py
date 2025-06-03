fbdbd10d553d07c669a6be18afff67d55eb17a72
zeeshan <zeeshanahmer@yahoo.com>
2025-06-02 23:28:58 -0400

    Initial first commit_6

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -14,6 +16,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
+st.text_input(label="", placeholder="Add a new todo....",
+              on_change=add_todo, key='new_todo')
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -23,8 +28,7 @@ for index, todo in enumerate(todos):
         st.rerun()
 
 
-st.text_input(label="", placeholder="Add a new todo....",
-              on_change=add_todo, key='new_todo')
+
 
 # print('Hello!')
 # st.session_state
